fa2d936bbfa8e556acdf0cf8071a5afd6dc039c1
f1scher7 <maksym.szylo@gmail.com>
2024-03-09 16:41:57 +0100

    Początek czegoś WIELKIEGO

---
+++ src/main/java/com/harmonylink/harmonylink/HarmonyLinkApplication.java
@@ -0,0 +1,13 @@
+package com.harmonylink.harmonylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmonyLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/harmonylink/harmonylink/HarmonyLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmonylink.harmonylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
