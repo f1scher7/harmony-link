ad2c1c2c9aaa1df324c7ba03085979ca2d24e4f5
f1scher7 <maksym.szylo@gmail.com>
2024-03-14 00:15:02 +0100

    Added generatePasswordResetToken func

--- src/main/java/com/harmonylink/harmonylink/services/user/useraccount/UserAccountUtils.java
+++ src/main/java/com/harmonylink/harmonylink/services/user/useraccount/UserAccountUtils.java
@@ -3,11 +3,17 @@ package com.harmonylink.harmonylink.services.user.useraccount;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 
 public class UserAccountUtils {
 
     public static String updateUserIpAddress(HttpServletRequest request) {
         return request.getRemoteAddr();
+    }
+
+    public static String generatePasswordResetToken() {
+        return UUID.randomUUID().toString();
     }
 
 }
