a3a965ec8dcf15b6a00103234ea6252a23f83a3e
f1scher7 <maksym.szylo@gmail.com>
2024-03-14 00:11:08 +0100

    Added tests for new functions for sending mails and reseting passwords

--- src/main/java/com/harmonylink/harmonylink/HarmonyLinkApplication.java
+++ src/main/java/com/harmonylink/harmonylink/HarmonyLinkApplication.java
@@ -32,22 +32,29 @@ public class HarmonyLinkApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        //Test registerUser func
+        //this.userAccountService.registerUserAccount(new UserAccount("kaziof1scher", "Kazio1234", "maksymilian.fischer7@gmail.com", LocalDate.of(2000, 7, 7), 'M'));
+
         /*
-        Registration of test users
+        //Registration of test users
         for (int i = 1; i < 4; i++) {
             Character sex = random.nextBoolean() ? 'M' : 'K';
             this.userAccountService.registerUserAccount(new UserAccount("testuser" + i, "testUser" + i, "testuser" + i + "@gmail.com", LocalDate.of(2000, 7, 7), sex));
         }
         */
 
-        //Test registerUser func
-        //this.userAccountService.registerUserAccount(new UserAccount("kaziof1scher", "Kazio1234", "maks.f1scher@gmail.com", LocalDate.of(2000, 7, 7), 'M'));
+        //Test loginUser func
+        //this.userAccountService.loginUserAccount("kaziof1scher", "Kazio7710");
 
-        // Test loginUser func
-        //this.userAccountService.loginUserAccount("maks.f1scher@gmail.com", "Kazio1234");
-
-        //Test updateData func
+        //Test updateUserAccountData func
         //this.userAccountService.updateUserData("testuser1", "4Qwe4q4w6e46q4w6e4", "qweqweji");
+
+        //Test resetUserAccountPassword
+        //this.userAccountService.resetUserAccountPassword("maksymilian.fischer7@gmail.com");
+
+        //Test changeUserAccountPassword
+        //UserAccount testUser = this.userAccountRepository.findByLogin("kaziof1scher");
+        //this.userAccountService.changeUserAccountPassword(testUser.getPasswordResetToken(), "Kazio7710");
 
         System.out.println();
         for (UserAccount userAccount : this.userAccountRepository.findAll()) {
