403a925181a7809daff202326405c71e9ec04635
f1scher7 <maksym.szylo@gmail.com>
2024-03-14 00:13:38 +0100

    Added new functions for sending mails and reseting passwords

--- src/main/java/com/harmonylink/harmonylink/services/user/useraccount/UserAccountService.java
+++ src/main/java/com/harmonylink/harmonylink/services/user/useraccount/UserAccountService.java
@@ -7,9 +7,12 @@ import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import static com.harmonylink.harmonylink.services.user.useraccount.UserAccountUtils.generatePasswordResetToken;
 import static com.harmonylink.harmonylink.services.user.useraccount.UserAccountValidator.isPasswordValid;
 import static com.harmonylink.harmonylink.services.user.useraccount.UserAccountValidator.isUserAtLeastSixteenYO;
 
@@ -21,12 +24,15 @@ public class UserAccountService {
 
     private final UserAccountRepository userAccountRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JavaMailSender javaMailSender;
 
     @Autowired
-    public UserAccountService(UserAccountRepository userAccountRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserAccountService(UserAccountRepository userAccountRepository, BCryptPasswordEncoder bCryptPasswordEncoder, JavaMailSender javaMailSender) {
         this.userAccountRepository = userAccountRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.javaMailSender = javaMailSender;
     }
+
 
     public void registerUserAccount(UserAccount userAccount) throws UserAlreadyExistsException, InvalidPasswordException, EmailAlreadyExistsException, InvalidGenderException, UserTooYoungException {
         if (userAccount == null) {
@@ -76,6 +82,10 @@ public class UserAccountService {
         }
     }
 
+    public void logoutUserAccount(HttpSession session) {
+        session.removeAttribute("user");
+        session.invalidate();
+    }
 
 
     public void updateUserData(String login, String newPassword, String newEmail) throws UserNotFoundException, InvalidPasswordException, EmailAlreadyExistsException {
@@ -107,8 +117,46 @@ public class UserAccountService {
         this.userAccountRepository.save(existingAccount);
     }
 
-    public void deleteUserAccountById(String id) {
-        this.userAccountRepository.deleteById(id);
+
+    public void resetUserAccountPassword(String email) {
+        UserAccount userAccount = this.userAccountRepository.findByEmail(email);
+        if (userAccount == null) {
+            throw new IllegalArgumentException("Email can't be null.");
+        }
+
+        userAccount.createPasswordResetToken();
+        this.userAccountRepository.save(userAccount);
+
+        SimpleMailMessage passwordResetEmail = new SimpleMailMessage();
+        String harmonyMail = System.getenv("HARMONYLINK_EMAIL");
+        passwordResetEmail.setFrom(harmonyMail);
+        passwordResetEmail.setTo(userAccount.getEmail());
+        passwordResetEmail.setSubject("Resetowanie hasła");
+        passwordResetEmail.setText("Aby zresetować swoje hasło, kliknij w link:\n\n" + "https://todo.com/reset-password?token=" + userAccount.getPasswordResetToken() + "\n\n" + "Jeśli nie prosiłeś o resetowanie hasła, zignoruj ten email lub skontaktuj się z pomocą techniczną, jeśli masz jakiekolwiek pytania.\n\n\n" + "Z poważaniem,\n" + "Twój HarmonyLink\n");
+
+        this.javaMailSender.send(passwordResetEmail);
+    }
+
+    public void changeUserAccountPassword(String token, String newPassword) throws InvalidPasswordException {
+        UserAccount userAccount = this.userAccountRepository.findByPasswordResetToken(token);
+        if (userAccount == null) {
+            throw new IllegalArgumentException("This token doesn't exist");
+        }
+
+        if (!isPasswordValid(newPassword)) {
+            throw new InvalidPasswordException();
+        }
+
+        userAccount.setPassword(bCryptPasswordEncoder.encode(newPassword));
+        userAccount.setPasswordResetToken(null);
+        userAccount.setPasswordResetTokenCreationDate(null);
+        this.userAccountRepository.save(userAccount);
+    }
+
+
+    public void deleteUserAccountByLogin(String login) {
+        this.userAccountRepository.deleteUserAccountByLogin(login);
+        UPDATE_USER_DATA_LOGGER.info("User with login: {} was deleted.", login);
     }
 
 }
