1d7e23e8be263034c05fb3d50f9c1e810d94995e
f1scher7 <maksym.szylo@gmail.com>
2024-03-15 23:46:23 +0100

    Added VerificationToken and ResetPasswordToken models

---
+++ src/main/java/com/harmonylink/harmonylink/models/user/token/ResetPasswordToken.java
@@ -0,0 +1,55 @@
+package com.harmonylink.harmonylink.models.user.token;
+
+import com.harmonylink.harmonylink.models.user.UserAccount;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@Document("reset_password_tokens")
+public class ResetPasswordToken {
+
+    @Id
+    private String id;
+    @Indexed(unique = true)
+    private String token;
+    @DBRef
+    private UserAccount userAccount;
+    @Field
+    @Indexed(expireAfterSeconds = 3600)
+    private LocalDateTime localDateTime;
+
+    public ResetPasswordToken(String token, UserAccount userAccount) {
+        this.token = token;
+        this.userAccount = userAccount;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getToken() {
+        return this.token;
+    }
+
+    public UserAccount getUserAccount() {
+        return this.userAccount;
+    }
+
+    public LocalDateTime getLocalDateTime() {
+        return this.localDateTime;
+    }
+
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+}
