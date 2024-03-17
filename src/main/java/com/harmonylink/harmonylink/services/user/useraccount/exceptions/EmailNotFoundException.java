3f3b57107592b143980419a6e49ec2ae9454e176
f1scher7 <maksym.szylo@gmail.com>
2024-03-17 23:15:51 +0100

    Added fixes for EmailNotFoundException

--- src/main/java/com/harmonylink/harmonylink/services/user/useraccount/exceptions/EmailNotFoundException.java
+++ src/main/java/com/harmonylink/harmonylink/services/user/useraccount/exceptions/EmailNotFoundException.java
@@ -3,7 +3,7 @@ package com.harmonylink.harmonylink.services.user.useraccount.exceptions;
 public class EmailNotFoundException extends Exception {
 
     public EmailNotFoundException() {
-        super("Nie możemy wysłać wiadmości z potwierdzeniem na ten mail");
+        super("Nie możemy wysłać wiadmości z potwierdzeniem na tego maila");
     }
 
 }
