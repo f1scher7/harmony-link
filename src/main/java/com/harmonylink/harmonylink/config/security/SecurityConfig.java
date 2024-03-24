b4dffa9db2726a9269291b63c2b9629ba5d44902
f1scher7 <maksym.szylo@gmail.com>
2024-03-24 23:39:49 +0100

    Added spring security configuration classes

--- src/main/java/com/harmonylink/harmonylink/config/security/SecurityConfig.java
+++ src/main/java/com/harmonylink/harmonylink/config/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.harmonylink.harmonylink.config;
+package com.harmonylink.harmonylink.config.security;
 
 import com.harmonylink.harmonylink.services.user.CustomUserAccountDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,48 +7,44 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     private final CustomUserAccountDetailsService userAccountDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
 
     @Autowired
-    public SecurityConfig(CustomUserAccountDetailsService userAccountDetailsService) {
+    public SecurityConfig(CustomUserAccountDetailsService userAccountDetailsService, PasswordEncoder passwordEncoder) {
         this.userAccountDetailsService = userAccountDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userAccountDetailsService).passwordEncoder(passwordEncoder);
     }
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/","/register","/login").permitAll() // Strony dostępne bez autoryzacji
+                        .requestMatchers("/","/registration","/login", "/forgot-password").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                        .loginPage("/") // Tutaj ustawiasz ścieżkę do Twojej strony logowania
-                        .defaultSuccessUrl("/dashboard", true) // Tutaj ustawiasz stronę, na którą użytkownik zostanie przekierowany po pomyślnym zalogowaniu
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/dashboard", true)
                         .permitAll()
                 );
         return http.build();
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userAccountDetailsService)
-                .passwordEncoder(passwordEncoder());
     }
 
 }
