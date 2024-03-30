9c58bb7bec8e47ec7820f02d8e0501f25d9a3559
f1scher7 <maksym.szylo@gmail.com>
2024-03-30 23:31:32 +0100

    Added autoLogin func for TEMP_USERS and delete method for them

--- src/main/java/com/harmonylink/harmonylink/services/user/useraccount/RegistrationService.java
+++ src/main/java/com/harmonylink/harmonylink/services/user/useraccount/RegistrationService.java
@@ -1,21 +1,37 @@
 package com.harmonylink.harmonylink.services.user.useraccount;
 
+import com.harmonylink.harmonylink.enums.Role;
 import com.harmonylink.harmonylink.models.user.UserAccount;
 import com.harmonylink.harmonylink.models.token.VerificationToken;
 import com.harmonylink.harmonylink.repositories.user.UserAccountRepository;
 import com.harmonylink.harmonylink.repositories.token.VerificationTokenRepository;
+import com.harmonylink.harmonylink.services.user.CustomUserAccountDetailsService;
 import com.harmonylink.harmonylink.services.user.useraccount.exceptions.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+import java.util.List;
 
 import static com.harmonylink.harmonylink.utils.UserAccountUtil.*;
 
 @Service
 public class RegistrationService {
 
+    private final CustomUserAccountDetailsService customUserAccountDetailsService;
+    private final AuthenticationManager authenticationManager;
     private final UserAccountRepository userAccountRepository;
     private final VerificationTokenRepository verificationTokenRepository;
     private final EmailService emailService;
@@ -23,7 +39,9 @@ public class RegistrationService {
 
 
     @Autowired
-    public RegistrationService(UserAccountRepository userAccountRepository, VerificationTokenRepository verificationTokenRepository, EmailService emailService, PasswordEncoder passwordEncoder) {
+    public RegistrationService(CustomUserAccountDetailsService customUserAccountDetailsService, AuthenticationManager authenticationManager, UserAccountRepository userAccountRepository, VerificationTokenRepository verificationTokenRepository, EmailService emailService, PasswordEncoder passwordEncoder) {
+        this.customUserAccountDetailsService = customUserAccountDetailsService;
+        this.authenticationManager = authenticationManager;
         this.userAccountRepository = userAccountRepository;
         this.verificationTokenRepository = verificationTokenRepository;
         this.emailService = emailService;
@@ -54,12 +72,28 @@ public class RegistrationService {
         }
 
         userAccount.setPassword(this.passwordEncoder.encode(userAccount.getPassword()));
+        userAccount.setRole(Role.TEMP_USER);
 
         String token = generateToken();
         VerificationToken verificationToken = new VerificationToken(token, userAccount);
 
-        this.emailService.sendVerificationEmail(userAccount, verificationToken.getToken());
+        this.userAccountRepository.save(userAccount);
         this.verificationTokenRepository.save(verificationToken);
+        this.emailService.sendVerificationEmail(userAccount, verificationToken.getToken());
+    }
+
+    public void autoLogin(String login, String password) {
+        UserAccount userAccount = (UserAccount) this.customUserAccountDetailsService.loadUserByUsername(login);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userAccount, password, userAccount.getAuthorities());
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        if (authentication.isAuthenticated()) {
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            HttpServletRequest newRequest = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+            HttpSession newSession = newRequest.getSession(true);
+            newSession.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+        }
     }
 
     @Transactional
@@ -76,8 +110,22 @@ public class RegistrationService {
             throw new InvalidTokenException();
         }
 
+        userAccount.setRole(Role.USER);
         this.userAccountRepository.save(userAccount);
         this.verificationTokenRepository.delete(verificationToken);
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void deleteUnconfirmedUserAccounts() {
+        List<UserAccount> userAccounts = this.userAccountRepository.findAllByRole(Role.TEMP_USER);
+
+        for (UserAccount userAccount: userAccounts) {
+            VerificationToken token = this.verificationTokenRepository.findByUserAccount(userAccount);
+
+            if (token == null) {
+                this.userAccountRepository.delete(userAccount);
+            }
+        }
     }
 
 }
