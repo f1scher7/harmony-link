c034378afd6f14944d0e80780b120608b69cf8cc
f1scher7 <maksym.szylo@gmail.com>
2024-04-01 22:03:59 +0200

    Added findByGoogle and findByRole funcs

--- src/main/java/com/harmonylink/harmonylink/repositories/user/UserAccountRepository.java
+++ src/main/java/com/harmonylink/harmonylink/repositories/user/UserAccountRepository.java
@@ -1,8 +1,11 @@
 package com.harmonylink.harmonylink.repositories.user;
 
+import com.harmonylink.harmonylink.enums.Role;
 import com.harmonylink.harmonylink.models.user.UserAccount;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserAccountRepository extends MongoRepository<UserAccount, String> {
@@ -10,6 +13,10 @@ public interface UserAccountRepository extends MongoRepository<UserAccount, Stri
     UserAccount findByLogin (String login);
 
     UserAccount findByEmail (String email);
+
+    UserAccount findByGoogleId(String googleId);
+
+    List<UserAccount> findAllByRole(Role role);
 
     void deleteUserAccountByLogin(String login);
 
