99a513df72a7f007b949d211bf59f5e37f50169e
f1scher7 <maksym.szylo@gmail.com>
2024-04-08 00:33:06 +0200

    Added UserProfileRepo

--- src/main/java/com/harmonylink/harmonylink/repositories/user/userprofile/UserProfileRepository.java
+++ src/main/java/com/harmonylink/harmonylink/repositories/user/userprofile/UserProfileRepository.java
@@ -1,8 +1,11 @@
-package com.harmonylink.harmonylink.repositories.user;
+package com.harmonylink.harmonylink.repositories.user.userprofile;
 
+import com.harmonylink.harmonylink.models.user.UserAccount;
 import com.harmonylink.harmonylink.models.user.userprofile.UserProfile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserProfileRepository extends MongoRepository<UserProfile, String> {
+
+    UserProfile findByUserAccount(UserAccount userAccount);
 
 }
