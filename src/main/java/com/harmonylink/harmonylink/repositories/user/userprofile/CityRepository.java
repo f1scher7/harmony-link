5845a444a2b30f874c6a51c56da902038b558669
f1scher7 <maksym.szylo@gmail.com>
2024-04-09 23:52:35 +0200

    Added findCitiesStartWith func

--- src/main/java/com/harmonylink/harmonylink/repositories/user/userprofile/CityRepository.java
+++ src/main/java/com/harmonylink/harmonylink/repositories/user/userprofile/CityRepository.java
@@ -1,10 +1,17 @@
 package com.harmonylink.harmonylink.repositories.user.userprofile;
 
 import com.harmonylink.harmonylink.models.user.userprofile.City;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface CityRepository extends MongoRepository<City, String> {
 
     City findByName(String name);
+
+    @Query("{ 'name': { '$regex': '^?0', '$options': 'i' } }")
+    List<City> findCitiesStartingWith(String prefix, Pageable pageable);
 
 }
