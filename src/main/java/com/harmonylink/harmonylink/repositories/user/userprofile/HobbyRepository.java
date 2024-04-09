4c9fa84a0a88c6f02747609e0d4600a8c87e05ef
f1scher7 <maksym.szylo@gmail.com>
2024-04-09 23:52:07 +0200

    Added findHobbiesStartWith func

--- src/main/java/com/harmonylink/harmonylink/repositories/user/userprofile/HobbyRepository.java
+++ src/main/java/com/harmonylink/harmonylink/repositories/user/userprofile/HobbyRepository.java
@@ -1,10 +1,17 @@
 package com.harmonylink.harmonylink.repositories.user.userprofile;
 
 import com.harmonylink.harmonylink.models.user.userprofile.Hobby;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface HobbyRepository extends MongoRepository<Hobby, String> {
 
     Hobby findByName(String name);
+
+    @Query("{ 'name': { '$regex': '?0', '$options': 'i' } }")
+    List<Hobby> findHobbiesStartingWith(String regex, Pageable pageable);
 
 }
