d11c0acaefc078a4274fde676292bbcf17674c19
f1scher7 <maksym.szylo@gmail.com>
2024-04-09 23:50:35 +0200

    Added tests for funcs form Hobby and City repos

--- src/main/java/com/harmonylink/harmonylink/HarmonyLinkApplication.java
+++ src/main/java/com/harmonylink/harmonylink/HarmonyLinkApplication.java
@@ -1,26 +1,19 @@
 package com.harmonylink.harmonylink;
 
-import com.harmonylink.harmonylink.enums.Role;
-import com.harmonylink.harmonylink.models.token.VerificationToken;
 import com.harmonylink.harmonylink.models.user.UserAccount;
-import com.harmonylink.harmonylink.models.user.userprofile.City;
-import com.harmonylink.harmonylink.models.user.userprofile.Hobby;
 import com.harmonylink.harmonylink.repositories.token.VerificationTokenRepository;
 import com.harmonylink.harmonylink.repositories.user.UserAccountRepository;
 import com.harmonylink.harmonylink.repositories.token.ResetPasswordTokenRepository;
 import com.harmonylink.harmonylink.repositories.user.userprofile.CityRepository;
 import com.harmonylink.harmonylink.repositories.user.userprofile.HobbyRepository;
+import com.harmonylink.harmonylink.services.user.userprofile.UserProfileService;
 import com.harmonylink.harmonylink.services.user.useraccount.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.core.userdetails.User;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
@@ -34,15 +27,18 @@ public class HarmonyLinkApplication implements CommandLineRunner {
     private final HobbyRepository hobbyRepository;
     private final CityRepository cityRepository;
     private final VerificationTokenRepository verificationTokenRepository;
+    private final UserProfileService userProfileService;
     private final RegistrationService registrationService;
 
+
     @Autowired
-    public HarmonyLinkApplication(UserAccountRepository userAccountRepository, ResetPasswordTokenRepository resetPasswordTokenRepository, HobbyRepository hobbyRepository, CityRepository cityRepository, VerificationTokenRepository verificationTokenRepository, RegistrationService registrationService) {
+    public HarmonyLinkApplication(UserAccountRepository userAccountRepository, ResetPasswordTokenRepository resetPasswordTokenRepository, HobbyRepository hobbyRepository, CityRepository cityRepository, VerificationTokenRepository verificationTokenRepository, UserProfileService userProfileService, RegistrationService registrationService) {
         this.userAccountRepository = userAccountRepository;
         this.resetPasswordTokenRepository = resetPasswordTokenRepository;
         this.hobbyRepository = hobbyRepository;
         this.cityRepository = cityRepository;
         this.verificationTokenRepository = verificationTokenRepository;
+        this.userProfileService = userProfileService;
         this.registrationService = registrationService;
     }
 
@@ -70,12 +66,23 @@ public class HarmonyLinkApplication implements CommandLineRunner {
         //ResetPasswordToken resetPasswordToken = this.resetPasswordTokenRepository.findByUserAccount(testUser);
         //this.userAccountService.setNewPasswordAfterReset(resetPasswordToken.getToken(), "Kazio1234");
 
+        /*
+        //Test for find funcs for Hobby and City
+        Pageable pageable = createPageableWithLimit(5);
+        List<City> cities = this.cityRepository.findCitiesStartingWith("kr", pageable);
+        List<Hobby> hobbies = this.hobbyRepository.findHobbiesStartingWith("pływanie", pageable);
+
+        System.out.println(cities);
+        System.out.println();
+        System.out.println(hobbies);
+        */
+
         System.out.println();
         for (UserAccount userAccount : this.userAccountRepository.findAll()) {
             System.out.println(userAccount);
         }
         System.out.println();
-        
+
     }
 
 }
