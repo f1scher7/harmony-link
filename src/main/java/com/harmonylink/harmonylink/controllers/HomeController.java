6983701e50c6a42e948bc64279d6ad31de9ddb4d
f1scher7 <maksym.szylo@gmail.com>
2024-04-16 23:40:20 +0200

    Added some fixes to home controller

--- src/main/java/com/harmonylink/harmonylink/controllers/HomeController.java
+++ src/main/java/com/harmonylink/harmonylink/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package com.harmonylink.harmonylink.controllers;
 
 import com.harmonylink.harmonylink.models.user.UserAccount;
+import com.harmonylink.harmonylink.models.user.userprofile.UserProfile;
 import com.harmonylink.harmonylink.repositories.user.UserAccountRepository;
+import com.harmonylink.harmonylink.repositories.user.userprofile.UserProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,40 +12,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 public class HomeController {
 
     private final UserAccountRepository userAccountRepository;
+    private final UserProfileRepository userProfileRepository;
 
     @Autowired
-    public HomeController(UserAccountRepository userAccountRepository) {
+    public HomeController(UserAccountRepository userAccountRepository, UserProfileRepository userProfileRepository) {
         this.userAccountRepository = userAccountRepository;
+        this.userProfileRepository = userProfileRepository;
     }
 
     @GetMapping
     public String showHomePage(Model model, Authentication authentication) {
+        UserAccount userAccount = null;
+
         if (authentication instanceof OAuth2AuthenticationToken oauthToken) {
             String email = oauthToken.getPrincipal().getAttribute("email");
-            model.addAttribute("username", email);
-
-            UserAccount userAccount = this.userAccountRepository.findByEmail(email);
-
-            model.addAttribute("googleId", userAccount.getGoogleId());
+            userAccount = this.userAccountRepository.findByEmail(email);
         } else if (authentication instanceof UsernamePasswordAuthenticationToken) {
             String username = authentication.getName();
-
-            model.addAttribute("username", username);
-
-            UserAccount userAccount = this.userAccountRepository.findByLogin(username);
-            List<String> ips = userAccount.getIpAddresses();
-
-            model.addAttribute("email", userAccount.getEmail());
-            model.addAttribute("password", userAccount.getPassword());
-            model.addAttribute("sex", userAccount.getSex());
-            model.addAttribute("ip", ips.get(0));
+            userAccount = this.userAccountRepository.findByLogin(username);
         }
+
+        UserProfile userProfile = this.userProfileRepository.findByUserAccount(userAccount);
+        model.addAttribute("userProfile", userProfile);
 
         return "home";
     }
