94cdf8acc539bb1c40898b0c94b7d1c6a7468f75
f1scher7 <maksym.szylo@gmail.com>
2024-04-19 23:52:09 +0200

    Added logic for main container for lg screens and added audio

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,8 +1,34 @@
+function adjustMainContainer() {
+    if (window.innerWidth > 1600) {
+        document.querySelector(".main-container").classList.add('container-fluid');
+        document.querySelector(".main-container").classList.add('mt-5');
+        document.querySelector('.main-container').classList.remove('container')
+    } else {
+        document.querySelector('.main-container').classList.remove('container-fluid');
+        document.querySelector('.main-container').classList.remove('mt-5');
+        document.querySelector('.main-container').classList.add('container')
+    }
+}
+
+window.onload = adjustMainContainer;
+window.onresize = adjustMainContainer;
+
+
 const videoElement = document.getElementById('user-camera');
+const audioElement = document.getElementById('user-audio');
+
 navigator.mediaDevices.getUserMedia( {video: true})
     .then(stream => {
         videoElement.srcObject = stream;
     })
     .catch(error => {
         console.error("Error accessing camera", error);
+    });
+
+navigator.mediaDevices.getUserMedia({audio: true})
+    .then(stream => {
+        audioElement.srcObject = stream;
+    })
+    .catch(error => {
+       console.error("Error accessing microphone", error);
     });
