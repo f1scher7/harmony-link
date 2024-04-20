9c3928598faeee2fc88591b2283b7a4c96500c94
f1scher7 <maksym.szylo@gmail.com>
2024-04-20 23:48:41 +0200

    Added methods for counting users activity

--- src/main/java/com/harmonylink/harmonylink/repositories/user/userprofile/UserProfileRepository.java
+++ src/main/java/com/harmonylink/harmonylink/repositories/user/userprofile/UserProfileRepository.java
@@ -3,9 +3,19 @@ package com.harmonylink.harmonylink.repositories.user.userprofile;
 import com.harmonylink.harmonylink.models.user.UserAccount;
 import com.harmonylink.harmonylink.models.user.userprofile.UserProfile;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface UserProfileRepository extends MongoRepository<UserProfile, String> {
 
     UserProfile findByUserAccount(UserAccount userAccount);
+
+    @Query(value = "{ 'activityStatus': {$in: ['ONLINE', 'IN_CALL', 'IN_SEARCH'] } }", count = true)
+    long countOnline();
+
+    @Query(value = "{ 'activityStatus': 'IN_CALL' }", count = true)
+    long countByInCallStatus();
+
+    @Query(value = "{ 'activityStatus': 'IN_SEARCH' }", count = true)
+    long countByInSearchStatus();
 
 }
