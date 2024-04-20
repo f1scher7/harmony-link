09a754b8e85a5b5779b68edfb4fb4d6225f1a3a5
f1scher7 <maksym.szylo@gmail.com>
2024-04-20 23:50:05 +0200

    Added methods for getting and setting users activity

--- src/main/java/com/harmonylink/harmonylink/controllers/HomeController.java
+++ src/main/java/com/harmonylink/harmonylink/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.harmonylink.harmonylink.controllers;
 
+import com.harmonylink.harmonylink.enums.UserActivityStatus;
 import com.harmonylink.harmonylink.models.user.UserAccount;
 import com.harmonylink.harmonylink.models.user.userprofile.UserProfile;
 import com.harmonylink.harmonylink.repositories.user.UserAccountRepository;
@@ -10,7 +11,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -18,11 +24,41 @@ public class HomeController {
     private final UserAccountRepository userAccountRepository;
     private final UserProfileRepository userProfileRepository;
 
+
     @Autowired
     public HomeController(UserAccountRepository userAccountRepository, UserProfileRepository userProfileRepository) {
         this.userAccountRepository = userAccountRepository;
         this.userProfileRepository = userProfileRepository;
     }
+
+
+    @GetMapping("/users-activity-status")
+    @ResponseBody
+    public List<Long> getUsersActivityStatus() {
+        List<Long> usersActivityStatus = new ArrayList<>();
+        usersActivityStatus.add(this.userProfileRepository.countOnline());
+        usersActivityStatus.add(this.userProfileRepository.countByInSearchStatus());
+        usersActivityStatus.add(this.userProfileRepository.countByInCallStatus());
+
+        return usersActivityStatus;
+    }
+
+    @PostMapping("/report-user-offline")
+    @ResponseBody
+    public void reportUserStatusOffline(@RequestBody Map<String, String> jsonBody) {
+        String userProfileId = jsonBody.get("userProfileId");
+
+        Optional<UserProfile> userProfileOptional = this.userProfileRepository.findById(userProfileId);
+        UserProfile userProfile = null;
+
+        if (userProfileOptional.isPresent()) {
+            userProfile = userProfileOptional.get();
+            System.out.println(userProfile);
+            userProfile.setActivityStatus(UserActivityStatus.OFFLINE);
+            this.userProfileRepository.save(userProfile);
+        }
+    }
+
 
     @GetMapping
     public String showHomePage(Model model, Authentication authentication) {
@@ -37,6 +73,9 @@ public class HomeController {
         }
 
         UserProfile userProfile = this.userProfileRepository.findByUserAccount(userAccount);
+        userProfile.setActivityStatus(UserActivityStatus.ONLINE);
+        this.userProfileRepository.save(userProfile);
+
         model.addAttribute("userProfile", userProfile);
 
         return "home";
