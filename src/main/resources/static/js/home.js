d6f5a771087b699323cf5397598ad43d50f01e3c
f1scher7 <maksym.szylo@gmail.com>
2024-04-20 23:51:32 +0200

    Added logic for users activity

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,34 +1,63 @@
-function adjustMainContainer() {
-    if (window.innerWidth > 1600) {
-        document.querySelector(".main-container").classList.add('container-fluid');
-        document.querySelector(".main-container").classList.add('mt-5');
-        document.querySelector('.main-container').classList.remove('container')
-    } else {
-        document.querySelector('.main-container').classList.remove('container-fluid');
-        document.querySelector('.main-container').classList.remove('mt-5');
-        document.querySelector('.main-container').classList.add('container')
+$(document).ready(function (url, data) {
+
+    function adjustMainContainer() {
+        if (window.innerWidth > 1600) {
+            document.querySelector(".main-container").classList.add('container-fluid');
+            document.querySelector(".main-container").classList.add('mt-5');
+            document.querySelector('.main-container').classList.remove('container')
+        } else {
+            document.querySelector('.main-container').classList.remove('container-fluid');
+            document.querySelector('.main-container').classList.remove('mt-5');
+            document.querySelector('.main-container').classList.add('container')
+        }
     }
-}
 
-window.onload = adjustMainContainer;
-window.onresize = adjustMainContainer;
+    $(window).on('load resize', adjustMainContainer);
 
 
-const videoElement = document.getElementById('user-camera');
-const audioElement = document.getElementById('user-audio');
+    const videoElement = $('#user-camera').get(0);
+    const audioElement = $('#user-audio').get(0);
 
-navigator.mediaDevices.getUserMedia( {video: true})
-    .then(stream => {
-        videoElement.srcObject = stream;
-    })
-    .catch(error => {
-        console.error("Error accessing camera", error);
+    navigator.mediaDevices.getUserMedia( {video: true, audio: true})
+        .then(stream => {
+            videoElement.srcObject = stream;
+            audioElement.srcObject = stream
+        })
+        .catch(error => {
+            console.error("Error accessing camera and microphone", error);
+        });
+
+
+    function fetchUsersActivityStatus() {
+        $.ajax({
+            url: '/users-activity-status',
+            type: 'GET',
+            success: function (data) {
+                $('#users-online').html(`<i class="fas fa-users"></i> Liczba użytkowników online: ${data[0]}`);
+                $('#users-in-search').html(`<i class="fas fa-search"></i> Liczba użytkowników szukających: ${data[1]}`);
+                $('#users-in-call').html(`<i class="fas fa-comments"></i> Liczba użytkowników w rozmowie: ${data[2]}`);
+            },
+            error: function (error) {
+                $('#users-online').html(`<i class="fas fa-users"></i> Liczba użytkowników online: null`);
+                $('#users-in-search').html(`<i class="fas fa-search"></i> Liczba użytkowników szukających: null`);
+                $('#users-in-call').html(`<i class="fas fa-comments"></i> Liczba użytkowników w rozmowie: null`);
+            }
+        })
+    }
+
+    fetchUsersActivityStatus()
+    setInterval(fetchUsersActivityStatus, 3000);
+
+
+    function reportUserStatusOffline (userProfileId) {
+        const data = JSON.stringify({userProfileId: userProfileId});
+        navigator.sendBeacon('/report-user-offline', new Blob([data], { type: 'application/json' }));
+
+    }
+
+    window.addEventListener('beforeunload', function (event) {
+        let userProfileId = $('#user-profile-id').text();
+        reportUserStatusOffline(userProfileId);
     });
 
-navigator.mediaDevices.getUserMedia({audio: true})
-    .then(stream => {
-        audioElement.srcObject = stream;
-    })
-    .catch(error => {
-       console.error("Error accessing microphone", error);
-    });
+})
