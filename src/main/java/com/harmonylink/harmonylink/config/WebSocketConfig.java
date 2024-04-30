06cf8d7c8ede104e0756476f0817ad42a2576f4c
f1scher7 <maksym.szylo@gmail.com>
2024-04-30 23:38:31 +0200

    Added constructor for WebSocketConfig

--- src/main/java/com/harmonylink/harmonylink/config/WebSocketConfig.java
+++ src/main/java/com/harmonylink/harmonylink/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.harmonylink.harmonylink.config;
 
-import com.harmonylink.harmonylink.handlers.websocket.HarmonyWebSocketHandler;
+import com.harmonylink.harmonylink.handlers.websocket.handler.HarmonyWebSocketHandler;
+import com.harmonylink.harmonylink.services.user.UserActivityStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
@@ -12,6 +14,15 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    private final UserActivityStatusService userActivityStatusService;
+
+
+    @Autowired
+    public WebSocketConfig(UserActivityStatusService userActivityStatusService) {
+        this.userActivityStatusService = userActivityStatusService;
+    }
+
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(harmonyWebSocket(), "/harmony-websocket");
@@ -19,7 +30,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Bean
     public WebSocketHandler harmonyWebSocket() {
-        return new HarmonyWebSocketHandler();
+        return new HarmonyWebSocketHandler(this.userActivityStatusService);
     }
 
 }
