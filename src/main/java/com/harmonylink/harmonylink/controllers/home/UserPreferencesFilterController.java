e97547fe8288afd9f89baeecd4775018445b1ffe
f1scher7 <maksym.szylo@gmail.com>
2024-05-04 23:27:10 +0200

    Added getAndSaveUserPreferences method

--- src/main/java/com/harmonylink/harmonylink/controllers/home/UserPreferencesFilterController.java
+++ src/main/java/com/harmonylink/harmonylink/controllers/home/UserPreferencesFilterController.java
@@ -1,7 +1,83 @@
 package com.harmonylink.harmonylink.controllers.home;
 
+import com.harmonylink.harmonylink.models.user.UserPreferencesFilter;
+import com.harmonylink.harmonylink.models.user.userprofile.UserProfile;
+import com.harmonylink.harmonylink.repositories.user.UserPreferencesFilterRepository;
+import com.harmonylink.harmonylink.repositories.user.userprofile.UserProfileRepository;
+import com.harmonylink.harmonylink.services.user.UserPreferencesFilterService;
+import com.harmonylink.harmonylink.services.user.userprofile.UserProfileService;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserPreferencesFilterController {
+
+    private final UserPreferencesFilterService userPreferencesFilterService;
+    private final UserProfileService userProfileService;
+    private final UserPreferencesFilterRepository userPreferencesFilterRepository;
+    private final UserProfileRepository userProfileRepository;
+
+
+    @Autowired
+    public UserPreferencesFilterController(UserPreferencesFilterService userPreferencesFilterService, UserProfileService userProfileService, UserPreferencesFilterRepository userPreferencesFilterRepository, UserProfileRepository userProfileRepository) {
+        this.userPreferencesFilterService = userPreferencesFilterService;
+        this.userProfileService = userProfileService;
+        this.userPreferencesFilterRepository = userPreferencesFilterRepository;
+        this.userProfileRepository = userProfileRepository;
+    }
+
+    @PostMapping("/user-preferences-data")
+    @ResponseBody
+    public void getAndSaveUserPreferences(@RequestBody String body) throws JSONException {
+        JSONObject jsonObject = new JSONObject(body);
+
+        JSONArray agesArray = jsonObject.optJSONArray("ages");
+        JSONArray heightsArray = jsonObject.optJSONArray("heights");
+        JSONArray citiesArray = jsonObject.optJSONArray("cities");
+        JSONArray hobbiesArray = jsonObject.optJSONArray("hobbies");
+        JSONArray studiesArray = jsonObject.optJSONArray("studies");
+
+        String userProfileId = jsonObject.optString("userProfileId");
+        String relationShipStatus = jsonObject.optString("relationshipStatus");
+        String sex = jsonObject.optString("sex");
+
+
+        List<Integer> ages = this.userPreferencesFilterService.getIntegerListFromJSON(agesArray);
+        List<Integer> heights = this.userPreferencesFilterService.getIntegerListFromJSON(heightsArray);
+        List<String> cities = this.userPreferencesFilterService.getStringListFromJSON(citiesArray);
+
+        List<String> hobbies = this.userPreferencesFilterService.getStringListFromJSON(hobbiesArray);
+        List<String> hobbyIds = this.userProfileService.getHobbyIds(hobbies);
+
+        List<String> studies = this.userPreferencesFilterService.getStringListFromJSON(studiesArray);
+
+        Optional<UserProfile> userProfileOptional = this.userProfileRepository.findById(userProfileId);
+
+        if (userProfileOptional.isPresent()) {
+            UserProfile userProfile = userProfileOptional.get();
+
+            UserPreferencesFilter userPreferencesFilter = this.userPreferencesFilterRepository.findByUserProfile(userProfile);
+
+            userPreferencesFilter.setUserProfile(userProfile);
+            userPreferencesFilter.setAges(ages);
+            userPreferencesFilter.setHeights(heights);
+            userPreferencesFilter.setCities(cities);
+            userPreferencesFilter.setFieldsOfStudy(studies);
+            userPreferencesFilter.setHobbyIds(hobbyIds);
+            userPreferencesFilter.setRelationshipStatus(relationShipStatus);
+            userPreferencesFilter.setSex(sex);
+
+            this.userPreferencesFilterRepository.save(userPreferencesFilter);
+        }
+    }
+
 }
