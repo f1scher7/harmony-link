a3346f2866eaca73e8b97dbd6d1330c582873ff2
f1scher7 <maksym.szylo@gmail.com>
2024-05-08 23:14:44 +0200

    Added functions folder for js home files

---
+++ src/main/resources/static/js/home/functions/utils.js
@@ -0,0 +1,55 @@
+export function adjustMainContainer() {
+    if (window.innerWidth > 1550) {
+        document.querySelector(".main-container").classList.add('container-fluid');
+        document.querySelector(".main-container").classList.add('mt-5');
+        document.querySelector('.main-container').classList.remove('container')
+    } else {
+        document.querySelector('.main-container').classList.remove('container-fluid');
+        document.querySelector('.main-container').classList.remove('mt-5');
+        document.querySelector('.main-container').classList.add('container')
+    }
+}
+
+export function checkCameraStatus (videoElement) {
+    if (videoElement && videoElement.srcObject) {
+        let tracks = videoElement.srcObject.getTracks();
+        if (tracks.length > 0) {
+            $('#user-camera-div').removeClass('h-100');
+        }
+    } else {
+        $('#user-camera-div').addClass('h-100')
+    }
+}
+
+export function fetchUsersActivityStatus() {
+    $.ajax({
+        url: '/users-activity-status',
+        type: 'GET',
+        success: function (data) {
+            $('#users-online').html(`<i class="fas fa-users"></i> Aktualna liczba użytkowników online: ${data[0]}`);
+            $('#users-in-search').html(`<i class="fas fa-sliders-h"></i> Ilość osób odpowiadających Twoim filtrom: ${data[1]}`);
+            $('#users-in-call').html(`<i class="fa fa-filter"></i> Ilość użytkowników szukających Ciebie: ${data[2]}`);
+        },
+        error: function (error) {
+            $('#users-online').html(`<i class="fas fa-users"></i> Aktualna liczba użytkowników online: error`);
+            $('#users-in-search').html(`<i class="fas fa-sliders-h"></i> Ilość osób odpowiadających Twoim filtrom: error`);
+            $('#users-in-call').html(`<i class="fa fa-filter"></i> Ilość użytkowników szukających Ciebie: error`);
+        }
+    })
+}
+
+export function sendUserProfileIdToController(endpoint) {
+    $.ajax({
+        url: endpoint,
+        type: 'POST',
+        data: {
+            userProfileId: window.userProfileId
+        },
+        success: function () {
+
+        },
+        error: function (error) {
+            console.error("Error while sending userProfileId: ", error);
+        }
+    })
+}
---
+++ src/main/resources/static/js/home/functions/websocketFuncs.js
@@ -0,0 +1,25 @@
+export function sendUserIdByWebsocket(websocket) {
+    if (websocket.readyState === WebSocket.OPEN) {
+        websocket.send(JSON.stringify({ type: 'USER_PROFILE_ID', userProfileId: window.userProfileId }));
+    } else {
+        console.error("WebSocket is not connected");
+    }
+}
+
+export function sendHearBeatByWebsocket(websocket) {
+    if (websocket.readyState === WebSocket.OPEN) {
+        websocket.send(JSON.stringify({ type: 'HEARTBEAT_REQUEST' }));
+    }
+}
+
+export function sendInSearchStatusByWebsocket(websocket) {
+    if (websocket.readyState === WebSocket.OPEN) {
+        websocket.send(JSON.stringify({ type: 'IN_SEARCH', userProfileId: window.userProfileId }));
+    }
+}
+
+export function sendStopSearchingStatusByWebsocket(websocket) {
+    if (websocket.readyState === WebSocket.OPEN) {
+        websocket.send(JSON.stringify({ type: 'STOP_SEARCHING', userProfileId: window.userProfileId }));
+    }
+}
