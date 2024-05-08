a630ef8fdbdab30248ad23c71d8971236cb1076d
f1scher7 <maksym.szylo@gmail.com>
2024-05-08 23:11:10 +0200

    Added MessageBroker and STOPM configs

--- src/main/java/com/harmonylink/harmonylink/config/WebSocketConfig.java
+++ src/main/java/com/harmonylink/harmonylink/config/WebSocketConfig.java
@@ -5,14 +5,14 @@ import com.harmonylink.harmonylink.services.user.UserActivityStatusService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBrokerConfigurer {
 
     private final UserActivityStatusService userActivityStatusService;
 
@@ -23,14 +23,26 @@ public class WebSocketConfig implements WebSocketConfigurer {
     }
 
 
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(harmonyWebSocket(), "/harmony-websocket");
-    }
-
     @Bean
     public WebSocketHandler harmonyWebSocket() {
         return new HarmonyWebSocketHandler(this.userActivityStatusService);
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(harmonyWebSocket(), "/harmony-websocket-handler");
+    }
+
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry messageBrokerRegistry) {
+        messageBrokerRegistry.enableSimpleBroker("/topic");
+        messageBrokerRegistry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/chat-connection").withSockJS();
     }
 
 }
