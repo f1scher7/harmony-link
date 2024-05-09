d55e1151992633fde4d18973ab5fdbcca8df500c
f1scher7 <maksym.szylo@gmail.com>
2024-05-09 23:46:41 +0200

    Added services and repos for initializing HarmonyWebSocketHandler

--- src/main/java/com/harmonylink/harmonylink/config/WebSocketConfig.java
+++ src/main/java/com/harmonylink/harmonylink/config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package com.harmonylink.harmonylink.config;
 
 import com.harmonylink.harmonylink.handlers.websocket.HarmonyWebSocketHandler;
-import com.harmonylink.harmonylink.services.user.UserActivityStatusService;
+import com.harmonylink.harmonylink.repositories.user.UserPreferencesFilterRepository;
+import com.harmonylink.harmonylink.repositories.user.userprofile.UserProfileRepository;
+import com.harmonylink.harmonylink.services.user.useractivitystatus.UserActivityStatusService;
+import com.harmonylink.harmonylink.services.user.useractivitystatus.UserInSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,17 +18,23 @@ import org.springframework.web.socket.config.annotation.*;
 public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBrokerConfigurer {
 
     private final UserActivityStatusService userActivityStatusService;
+    private final UserInSearchService userInSearchService;
+    private final UserProfileRepository userProfileRepository;
+    private final UserPreferencesFilterRepository userPreferencesFilterRepository;
 
 
     @Autowired
-    public WebSocketConfig(UserActivityStatusService userActivityStatusService) {
+    public WebSocketConfig(UserActivityStatusService userActivityStatusService, UserInSearchService userInSearchService, UserProfileRepository userProfileRepository, UserPreferencesFilterRepository userPreferencesFilterRepository) {
         this.userActivityStatusService = userActivityStatusService;
+        this.userInSearchService = userInSearchService;
+        this.userProfileRepository = userProfileRepository;
+        this.userPreferencesFilterRepository = userPreferencesFilterRepository;
     }
 
 
     @Bean
     public WebSocketHandler harmonyWebSocket() {
-        return new HarmonyWebSocketHandler(this.userActivityStatusService);
+        return new HarmonyWebSocketHandler(this.userActivityStatusService, this.userInSearchService, this.userProfileRepository, this.userPreferencesFilterRepository);
     }
 
     @Override
