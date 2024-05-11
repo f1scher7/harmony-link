0c2a4cb16868d900f357f92aacffe71a34965520
f1scher7 <maksym.szylo@gmail.com>
2024-05-11 22:54:21 +0200

    Added js file for webrtc funcs

---
+++ src/main/resources/static/js/home/functions/webrtcFuncs.js
@@ -0,0 +1,44 @@
+export function handleVideoOfferMsg(msg) {
+    const desc = new RTCSessionDescription(msg.sdp);
+
+    window.localPeerConnection.setRemoteDescription(desc)
+        .then(() => window.localPeerConnection.createAnswer())
+        .then(answer => window.localPeerConnection.setLocalDescription(answer))
+        .then(() => {
+            const answerMsg = {
+                type: 'VIDEO_ANSWER',
+                sdp: window.localPeerConnection.localDescription
+            };
+            window.websocket.send(JSON.stringify(answerMsg));
+        })
+        .catch(error => {
+            console.error('Error during handleVideoOfferMsg:', error);
+        });
+}
+
+export function handleVideoAnswerMsg(msg) {
+    const desc = new RTCSessionDescription(msg.sdp);
+
+    window.localPeerConnection.setRemoteDescription(desc)
+        .catch(error => {
+            console.error('Error during handleVideoAnswerMsg:', error);
+        });
+}
+
+export function handleNewICECandidateMsg(msg) {
+    const candidate = new RTCIceCandidate(msg.candidate);
+
+    window.localPeerConnection.addIceCandidate(candidate)
+        .catch(error => {
+            console.error('Error during handleNewICECandidateMsg:', error);
+        })
+}
+
+export function sendCandidateToPeer(candidate) {
+    const candidateMsg = {
+        type: 'NEW_ICE_CANDIDATE',
+        candidate: candidate
+    };
+
+    window.websocket.send(JSON.stringify(candidateMsg));
+}
