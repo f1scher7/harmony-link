5576aa74a8d3373b5a5cd3ec074a134ab1390bac
f1scher7 <maksym.szylo@gmail.com>
2024-05-11 22:55:16 +0200

    Added new messages types and some fixes

--- src/main/resources/static/js/home/home.js
+++ src/main/resources/static/js/home/home.js
@@ -1,5 +1,6 @@
 import { adjustMainContainer, checkCameraStatus, fetchUsersActivityStatus } from './functions/utils.js';
 import { sendUserIdByWebsocket, sendHearBeatByWebsocket } from './functions/websocketFuncs.js';
+import { handleVideoOfferMsg, handleVideoAnswerMsg, handleNewICECandidateMsg } from "./functions/webrtcFuncs.js";
 
 $(document).ready(function (url, data) {
 
@@ -18,7 +19,19 @@ $(document).ready(function (url, data) {
     }
 
     window.websocket.onmessage = function (event) {
-        console.log("Received message: " + event.data);
+        const message = JSON.parse(event.data);
+
+        switch (message.type) {
+            case 'VIDEO_OFFER':
+                handleVideoOfferMsg(message);
+                break;
+            case 'VIDEO_ANSWER':
+                handleVideoAnswerMsg(message);
+                break;
+            case 'NEW_ICE_CANDIDATE':
+                handleNewICECandidateMsg(message);
+                break;
+        }
     }
 
     window.websocket.onclose = function (event) {
@@ -32,26 +45,26 @@ $(document).ready(function (url, data) {
     setInterval(() => sendHearBeatByWebsocket(window.websocket), 30000);
 
 
-    const videoElement = $('#user-camera').get(0);
-    const audioElement = $('#user-audio').get(0);
+    const localVideoElement = $('#local-camera').get(0);
+    const localAudioElement = $('#local-audio').get(0);
 
     navigator.mediaDevices.getUserMedia( {video: true, audio: true})
         .then(stream => {
-            videoElement.srcObject = stream;
-            audioElement.srcObject = stream;
+            localVideoElement.srcObject = stream;
+            localAudioElement.srcObject = stream;
             $('#camera-error').addClass('d-none');
         })
         .catch(error => {
             console.error("Error accessing camera and microphone", error);
             $('#user-camera-div').addClass('info-div');
-            $('#user-camera').remove();
+            $('#local-camera').remove();
             $('#camera-error').removeClass('d-none');
             $('#start-btn').addClass('disabled');
             $('#filters-btn').addClass('disabled')
         });
 
-    checkCameraStatus(videoElement);
-    videoElement.onplaying = (()=> checkCameraStatus(videoElement));
+    checkCameraStatus(localVideoElement);
+    localVideoElement.onplaying = (()=> checkCameraStatus(localVideoElement));
 
 
     fetchUsersActivityStatus()
