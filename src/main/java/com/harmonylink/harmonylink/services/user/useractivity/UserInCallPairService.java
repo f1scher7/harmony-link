805296e8ac596c913b1048a1dd58afd0cfa236e3
f1scher7 <maksym.szylo@gmail.com>
2024-05-15 23:56:24 +0200

    Added getAnotherUserProfileByUserProfileId and removeUserCallPairDataByUserProfileId funcs for stopping conn logic

--- src/main/java/com/harmonylink/harmonylink/services/user/useractivity/UserInCallPairService.java
+++ src/main/java/com/harmonylink/harmonylink/services/user/useractivity/UserInCallPairService.java
@@ -1,7 +1,10 @@
 package com.harmonylink.harmonylink.services.user.useractivity;
 
+import com.harmonylink.harmonylink.enums.UserActivityStatusEnum;
 import com.harmonylink.harmonylink.models.user.userdata.UserCallPairData;
 import com.harmonylink.harmonylink.models.user.userprofile.UserProfile;
+import com.harmonylink.harmonylink.services.user.userprofile.exceptions.UserProfileDoesntExistException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,6 +17,35 @@ import static com.harmonylink.harmonylink.utils.UserUtil.getUserProfilesId;
 public class UserInCallPairService {
 
     private final ConcurrentHashMap<String, UserCallPairData> inCallUsersPairs = new ConcurrentHashMap<>();
+    private final UserActivityStatusService userActivityStatusService;
+
+
+    @Autowired
+    public UserInCallPairService(UserActivityStatusService userActivityStatusService) {
+        this.userActivityStatusService = userActivityStatusService;
+    }
+
+
+    public UserCallPairData getUserCallPairData(UserProfile userProfile1, UserProfile userProfile2) {
+        return inCallUsersPairs.get(getUserProfilesId(userProfile1, userProfile2));
+    }
+
+    public UserProfile getAnotherUserProfileByUserProfileId(String userProfileId) {
+        for (UserCallPairData userCallPairData: inCallUsersPairs.values()) {
+            if (userCallPairData.getUserProfile1().getId().equals(userProfileId)) {
+                return userCallPairData.getUserProfile2();
+            } else if (userCallPairData.getUserProfile2().getId().equals(userProfileId)) {
+                return  userCallPairData.getUserProfile1();
+            }
+        }
+
+        return null;
+    }
+
+    public List<UserCallPairData> getAllUserCallPairData() {
+        return new ArrayList<>(inCallUsersPairs.values());
+    }
+
 
     public void addUserCallPairData(UserProfile userProfile1, UserProfile userProfile2) {
         if (userProfile1 != null && userProfile2 != null) {
@@ -21,21 +53,32 @@ public class UserInCallPairService {
         }
     }
 
-    public UserCallPairData getUserCallPairData(UserProfile userProfile1, UserProfile userProfile2) {
-        return inCallUsersPairs.get(getUserProfilesId(userProfile1, userProfile2));
-    }
-
     public List<UserCallPairData> getAllUserCallInPairData() {
         return new ArrayList<>(inCallUsersPairs.values());
     }
+
 
     public void removeUserCallPairData(UserProfile userProfile1, UserProfile userProfile2) {
         inCallUsersPairs.remove(getUserProfilesId(userProfile1, userProfile2));
     }
 
+    public void removeUserCallPairDataByUserProfileId(String userProfileId) throws UserProfileDoesntExistException {
+        for (UserCallPairData userCallPairData : inCallUsersPairs.values()) {
+            if (userCallPairData.getUserProfile1().getId().equals(userProfileId) || userCallPairData.getUserProfile2().getId().equals(userProfileId)) {
+                removeUserCallPairData(userCallPairData.getUserProfile1(), userCallPairData.getUserProfile2());
+
+                this.userActivityStatusService.updateUserActivityStatusInDB(userCallPairData.getUserProfile1().getId(), UserActivityStatusEnum.ONLINE);
+                this.userActivityStatusService.updateUserActivityStatusInDB(userCallPairData.getUserProfile2().getId(), UserActivityStatusEnum.ONLINE);
+
+                break;
+            }
+        }
+    }
+
     public void removeAllUserCallPairData() {
         inCallUsersPairs.clear();
     }
+
 
     public String displayInSearchUsers() {
         StringBuilder builder = new StringBuilder();
