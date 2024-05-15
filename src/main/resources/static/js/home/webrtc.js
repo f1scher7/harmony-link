f6ead63f50b78b6e3b679e69cd037c2424193cee
f1scher7 <maksym.szylo@gmail.com>
2024-05-16 00:05:58 +0200

    Added basic logic for webrtc connection state

--- src/main/resources/static/js/home/webrtc.js
+++ src/main/resources/static/js/home/webrtc.js
@@ -1,21 +1,15 @@
-import { sendCandidateToPeer } from "./functions/webrtcFuncs.js";
+import { createPeerConnection, sendCandidateToPeer } from "./functions/webrtcFuncs.js";
+import { fetchUsersActivityStatus } from "./functions/utils.js";
 
 $(document).ready(function () {
-    const configIceServers = {
-        iceServers: [
-            {
-                urls: 'stun:stun.l.google.com:19302'
-            },
 
-            {
-                urls: 'turn:openrelay.metered.ca:443',
-                username: 'openrelayproject',
-                credential: 'openrelayproject'
-            }
-        ]
-    }
+    const startBtn = $('#start-btn');
+    const stopBtn = $('#stop-btn');
+    const filtersBtn = $('#filters-btn');
+    const statisticDataDiv = $('.statistics-data');
+    const hlLogoInInfoDiv = $('.hlLogoInInfoDiv');
 
-    window.localPeerConnection = new RTCPeerConnection(configIceServers);
+    createPeerConnection();
 
     window.localPeerConnection.onicecandidate = function(event) {
         try {
@@ -48,9 +42,41 @@ $(document).ready(function () {
 
             case 'disconnected':
             case "failed":
+                window.localPeerConnection.close();
+                window.localPeerConnection = null;
+
+                createPeerConnection();
+
+
                 $('.main-container').removeClass('mb-2');
                 $('.main-info-div').removeClass('d-none');
                 $('.main-remote-user-div').addClass('d-none');
+
+                if (window.innerHeight > 1600) {
+                    hlLogoInInfoDiv.removeClass('mb-5');
+                    hlLogoInInfoDiv.addClass('mb-2');
+                } else {
+                    hlLogoInInfoDiv.removeClass('mb-2');
+                    hlLogoInInfoDiv.addClass('mb-5');
+                }
+
+                statisticDataDiv.empty();
+                statisticDataDiv.html(`
+                        <div class="d-inline-block">
+                            <h5 class="font-weight-bold" id="users-online"><i class="fas fa-users"></i> Aktualna liczba użytkowników online: </h5>
+                            <h5 class="font-weight-bold" id="users-in-search"><i class="fas fa-sliders-h"></i> Ilość osób odpowiadających Twoim filtrom: </h5>
+                            <h5 class="font-weight-bold" id="users-in-call"><i class="fa fa-filter"></i> Ilość użytkowników szukających Ciebie:</h5>
+                        </div>
+                    `);
+
+                fetchUsersActivityStatus();
+
+                window.userActivityInterval = setInterval(fetchUsersActivityStatus, 3000);
+
+                stopBtn.addClass('disabled');
+                startBtn.removeClass('disabled');
+                filtersBtn.removeClass('disabled');
+
                 break;
 
             case "closed":
