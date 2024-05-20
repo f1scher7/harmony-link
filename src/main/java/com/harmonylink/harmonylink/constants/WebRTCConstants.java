906bed1eef5915b08c716bde1c64c05bcfdea10e
f1scher7 <maksym.szylo@gmail.com>
2024-05-20 23:58:52 +0200

    Added constants for WebRTC and WebSocket

---
+++ src/main/java/com/harmonylink/harmonylink/constants/WebRTCConstants.java
@@ -0,0 +1,7 @@
+package com.harmonylink.harmonylink.constants;
+
+public class WebRTCConstants {
+
+    public static final String INITIATE_OFFER_JSON = "{\"type\":\"INITIATE_OFFER\"}";
+
+}
---
+++ src/main/java/com/harmonylink/harmonylink/constants/WebsocketConstants.java
@@ -0,0 +1,9 @@
+package com.harmonylink.harmonylink.constants;
+
+public final class WebsocketConstants {
+
+    public static final String HEARTBEAT_REQUEST_JSON = "{\"type\":\"HEARTBEAT_REQUEST\"}";
+    public static final String HEARTBEAT_RESPONSE_JSON = "{\"type\":\"HEARTBEAT_RESPONSE\"}";
+    public static final String TALKER_NICKNAME_JSON = "{\"type\":\"TALKER_NICKNAME\",\"nickname\":\"%s\"}";
+
+}
