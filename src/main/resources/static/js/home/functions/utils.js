0114edec31f51213abb5474230c05410f5e0556a
f1scher7 <maksym.szylo@gmail.com>
2024-05-22 11:14:35 +0200

    Added setMainInfoDivAfterCall func

--- src/main/resources/static/js/home/functions/utils.js
+++ src/main/resources/static/js/home/functions/utils.js
@@ -1,11 +1,9 @@
 export function adjustMainContainer() {
     if (window.innerWidth > 1550) {
         document.querySelector(".main-container").classList.add('container-fluid');
-        document.querySelector(".main-container").classList.add('mt-5');
         document.querySelector('.main-container').classList.remove('container')
     } else {
         document.querySelector('.main-container').classList.remove('container-fluid');
-        document.querySelector('.main-container').classList.remove('mt-5');
         document.querySelector('.main-container').classList.add('container')
     }
 }
@@ -36,6 +34,37 @@ export function fetchUsersActivityStatus() {
             $('#users-in-call').html(`<i class="fa fa-filter"></i> Ilość użytkowników szukających Ciebie: error`);
         }
     })
+}
+
+export function setMainInfoDivAfterCall(mainInfoDiv) {
+    mainInfoDiv.empty();
+    mainInfoDiv.html(`
+        <div class="info-div h-100 d-flex justify-content-center align-items-center">
+           <div class="row">
+               <div class="w-100 hlLogoInInfoDiv d-flex justify-content-center mt-3 mb-5">
+                   <img src="/img/logo/coloredLogoHarmonyLink.png" class="img-fluid w-50" alt="" oncontextmenu="return false;">
+               </div>
+               <div class="col-12 statistics-data d-flex justify-content-center mt-5 mb-5">
+                   <div class="d-inline-block">
+                       <h5 class="font-weight-bold" id="users-online">
+                       </h5>
+                       <h5 class="font-weight-bold" id="users-in-search">
+                       </h5>
+                       <h5 class="font-weight-bold" id="users-in-call">
+                       </h5>
+                   </div>
+               </div>
+               <div class="col-md-12 mb-5 mt-4">
+               </div>
+               <div class="w-100 d-flex justify-content-center mt-5">
+                   <div class="text-center w-25">
+                       <img src="/img/logo/blueLogoFischer.png" class="img-fluid w-75" alt="" oncontextmenu="return false;">
+                       <h6 class="text-center mt-2">2024</h6>
+                   </div>
+               </div>
+           </div>
+       </div>
+    `);
 }
 
 export function setDefaultInfoDiv(hlLogoInInfoDiv, statisticDataDiv, startBtn, stopBtn, filtersBtn) {
