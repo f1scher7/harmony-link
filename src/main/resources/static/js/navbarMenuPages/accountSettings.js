561c859a3f5234b13fb79403d399073b8cc99092
f1scher7 <maksym.szylo@gmail.com>
2024-06-05 01:17:41 +0200

    Added logic for displaying modals

---
+++ src/main/resources/static/js/navbarMenuPages/accountSettings.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    if (displayChangePasswordSuccessModal !== null && displayChangePasswordSuccessModal.toString() === "1") {
+        $('#changePasswordSuccessModal').modal('show');
+    }
+
+    if (displayChangeEmailModal !== null && displayChangeEmailModal.toString() === "1") {
+        $('#changeEmailModal').modal('show');
+    }
+
+})
