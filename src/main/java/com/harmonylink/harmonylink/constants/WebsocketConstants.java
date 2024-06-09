4ddfc4497e903c93d27c7b9f6241804dffa914ad
f1scher7 <maksym.szylo@gmail.com>
2024-06-09 16:49:48 +0200

    Added SEND_MESSAGE_FOR_RECIPIENT_JSON

--- src/main/java/com/harmonylink/harmonylink/constants/WebsocketConstants.java
+++ src/main/java/com/harmonylink/harmonylink/constants/WebsocketConstants.java
@@ -5,5 +5,6 @@ public final class WebsocketConstants {
     public static final String HEARTBEAT_REQUEST_JSON = "{\"type\":\"HEARTBEAT_REQUEST\"}";
     public static final String HEARTBEAT_RESPONSE_JSON = "{\"type\":\"HEARTBEAT_RESPONSE\"}";
     public static final String TALKER_NICKNAME_JSON = "{\"type\":\"TALKER_NICKNAME\",\"nickname\":\"%s\"}";
+    public static final String SEND_MESSAGE_FOR_RECIPIENT_JSON = "{\"type\":\"SEND_MESSAGE_FOR_RECIPIENT\",\"mess\":\"%s\"}";
 
 }
